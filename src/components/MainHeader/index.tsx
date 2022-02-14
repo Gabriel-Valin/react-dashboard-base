4909d341d055671ead6cb302898532c2dbbb4b41
Gabriel da Costa Valin <gvt3ch@Gabriels-MacBook-Air.local>
2022-02-14 13:42:42 -0300

    feat: start Profile component Welcome & Username

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,20 @@
-import { Container } from "./styles";
+import { useMemo } from "react";
+import emojis from "../../utils/emojis";
+import { Container, Welcome, Username, Profile } from "./styles";
 
 export default function MainHeader () {
+    const sortEmoji = useMemo(() => {
+        const index = Math.floor(Math.random() * emojis.length)
+        return emojis[index]
+    }, [])
+
     return (
         <Container>
-            Main Header
+            <h1>toggle</h1>
+            <Profile>
+                <Welcome>Ola, {sortEmoji} </Welcome>
+                <Username>Gabriel Valin</Username>
+            </Profile>
         </Container>
     )
 }
