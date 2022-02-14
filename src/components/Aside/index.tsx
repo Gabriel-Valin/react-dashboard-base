8f64372aa9d2a575e01b6aac8274148daf71f027
Gabriel da Costa Valin <gvt3ch@Gabriels-MacBook-Air.local>
2022-02-14 14:39:01 -0300

    feat: aside menu complete

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,34 @@
-import { Container } from "./styles";
+import { Container, Header, LogoImg, MenuNavigator, MenuItemLink, TitleHeader } from "./styles";
+import logoImg from '../../assets/logo.svg';
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from 'react-icons/md';
 
 export default function Aside () {
     return (
         <Container>
-            Aside
+            <Header>
+                <LogoImg src={logoImg} alt="Logo Application" />
+                <TitleHeader>
+                    Minha Carteira
+                </TitleHeader>
+            </Header>
+            <MenuNavigator>
+                <MenuItemLink href="#">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowDownward />
+                    Saidas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdExitToApp />
+                    Sair
+                </MenuItemLink>
+            </MenuNavigator>
         </Container>
     )
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,4 +3,49 @@ import styled from "styled-components";
 export const Container = styled.div`
     grid-area: AS;
     background-color: ${props => props.theme.colors.secondary};
+    padding-left: 20px;
+
+    border-right: 1px solid ${props => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+
+    height: 80px;
+`;
+
+export const LogoImg  = styled.img`
+    height: 40px;
+    width: 40px;
+`;
+
+export const TitleHeader = styled.h3`
+    color: ${props => props.theme.colors.white};
+    margin-left: 15px;
+`;
+
+export const MenuNavigator  = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+`;
+
+export const MenuItemLink = styled.a`
+    display: flex;
+    align-items: center;
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+
+    margin: 10px 0;
+
+    transition: opacity .3s;
+
+    &:hover {
+        opacity: .7;
+    }
+    
+    > svg {
+        margin-right: 5px;
+    }
 `;
