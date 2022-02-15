c42500c99f77b82f9abe6c60fa43460541e0eff9
Gabriel da Costa Valin <gvt3ch@Gabriels-MacBook-Air.local>
2022-02-15 11:40:10 -0300

    feat: personalize scrollbar with ::-webkit

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -17,6 +17,28 @@ function List() {
 
             <Content>
                 <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
+                <FinanceCard cardColor='#313862' tagColor="#e44c4e" title="Conta de luz" subtitle="27/07/2022" amount="R$ 130,00"/>
+
             </Content>
         </Container>
     );
