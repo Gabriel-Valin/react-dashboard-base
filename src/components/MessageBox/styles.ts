31ffc756384d616f4b0c6cfe5b133c6a606c09c2
Gabriel da Costa Valin <gvt3ch@Gabriels-MacBook-Air.local>
2022-02-20 00:31:33 -0300

    feat: dynamic messages and WalletBox infos

---
+++ src/components/MessageBox/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 48%;
+    color: ${props => props.theme.colors.white};
+
+    height: 260px;
+    background-color: ${props  => props.theme.colors.tertiary};
+
+    border-radius: 10px;
+
+    margin: 10px 0;
+    padding: 30px 20px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    > header img {
+        height: 35px;
+        width: auto;
+    }
+
+    > header p {
+        font-size: 18px;
+    }
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,6 +7,12 @@ import gains from "../../repositories/gains";
 import expenses from "../../repositories/expenses";
 import listMonth from '../../utils/months';
 import WalletBox from "../../components/WalletBox";
+import MessageBox from "../../components/MessageBox";
+
+import happyimg from '../../assets/happy.svg';
+import sadimg from '../../assets/sad.svg';
+import grinningimg from '../../assets/grinning.svg';
+
 
 export default function Dashboard () {
     const [mounthSelected, setMounthSelected] = useState<string>(String(new Date().getMonth() + 1));
@@ -40,6 +46,78 @@ export default function Dashboard () {
         })
     }, [])
 
+    const totalExpenses = useMemo(() => {
+        let total: number = 0;
+
+        expenses.forEach(expense => {
+            const date = new Date(expense.date);
+            const year = String(date.getFullYear());
+            const month = String(date.getMonth() + 1);
+
+            if (month === mounthSelected && year === yearSelected) {
+                try {
+                    total += Number(expense.amount)
+                } catch (error) {
+                    throw new Error('invalid amount, must be number.')
+                }
+            }
+        });
+
+        return total
+    }, [mounthSelected, yearSelected])
+
+    const totalGains = useMemo(() => {
+        let total: number = 0;
+
+        gains.forEach(gain => {
+            const date = new Date(gain.date);
+            const year = String(date.getFullYear());
+            const month = String(date.getMonth() + 1);
+
+            if (month === mounthSelected && year === yearSelected) {
+                try {
+                    total += Number(gain.amount)
+                } catch (error) {
+                    throw new Error('invalid amount, must be number.')
+                }
+            }
+        });
+
+        return total
+    }, [mounthSelected, yearSelected])
+
+    const totalBalance = useMemo(() => {
+        return (+totalGains - totalExpenses)
+    }, [totalExpenses, totalExpenses])
+
+    const messageAboutBalance = useMemo(() => {
+        if (totalBalance < 0) {
+            return {
+                title: 'Poxa, vei!',
+                description: 'Tu gasto muito nesse mes, ate mais do que deveria, se liga em!',
+                footerText: 'Verifica seus gastos e tenta fazer um controle',
+                icon: sadimg
+            }
+        }
+
+        if (totalBalance === 0) {
+            return {
+                title: 'UFAAAA!!',
+                description: 'Tu gastou exatamente o que ganhou, calma la em',
+                footerText: 'Cuidado!',
+                icon: grinningimg
+            }
+        }
+
+        return {
+            title: 'Irrrra!!',
+            description: 'Esse mes tu ficou positivo em !!',
+            footerText: 'Continua assim!!',
+            icon: happyimg
+        }
+        
+    }, [totalBalance])
+
     return (
        <Container>
             <ContentHeader title="Dashboard" lineColor="#f73">
@@ -47,9 +125,11 @@ export default function Dashboard () {
                 <SelectInput options={years} onChange={(e) => setYearSelected(e.target.value)} defaultValue={yearSelected} />
             </ContentHeader>
             <Content>
-                <WalletBox title="saldo" amount={150.00} footerLabel="Entradas e Saidas" icon="dolar" color="#4e4"/>
-                <WalletBox title="entradas" amount={5000.00} footerLabel="Entradas e Saidas" icon="arrowup" color="#f7931b"/>
-                <WalletBox title="saidas" amount={4850.00} footerLabel="Entradas e Saidas" icon="arrowdown" color="#e44e"/>
+                <WalletBox title="saldo" amount={totalBalance} footerLabel="Entradas e Saidas" icon="dolar" color="#4e4"/>
+                <WalletBox title="entradas" amount={totalGains} footerLabel="Entradas e Saidas" icon="arrowup" color="#f7931b"/>
+                <WalletBox title="saidas" amount={totalExpenses} footerLabel="Entradas e Saidas" icon="arrowdown" color="#e44e"/>
+
+                <MessageBox title={messageAboutBalance.title} description={messageAboutBalance.description} footerText={messageAboutBalance.footerText} icon={messageAboutBalance.icon} />
             </Content>
        </Container>
     )
